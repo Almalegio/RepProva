113185acc80666cb2f7b45689ec608298f0d6278
Almalegio <71653326+Almalegio@users.noreply.github.com>
2020-10-08 16:47:07 +0200

    Update Entita1.cs

--- ProgAlma/Entity/Entita1.cs
+++ ProgAlma/Entity/Entita1.cs
@@ -7,7 +7,7 @@ namespace ProgAlma.Entity
     class Entita1
     {
         string appo = "Test Piero ";
-         string appo2 = "Test Piero";
+         string appo2 = "Test Piero 55";
          string appo3 = "Test Piero 1";
     }
 }
